4af935ed893ad24507506a4159108ad94d285eed
yongho1212 <yongho6800@gmail.com>
2021-05-17 21:15:01 +0900

    navigate

---
+++ src/Screens/ProfileMaking/Else/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const aboutMe = () => {
+     return(
+          <View>
+               <Text>
+                    about me
+               </Text>
+          </View>
+     )
+}
+ 
+export default aboutMe;
